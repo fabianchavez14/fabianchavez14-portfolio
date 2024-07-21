d62a554319f5bf812fe087190c2c18da456e7407
fabianchavez14 <99099722+fabianchavez14@users.noreply.github.com>
2024-07-21 07:19:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
